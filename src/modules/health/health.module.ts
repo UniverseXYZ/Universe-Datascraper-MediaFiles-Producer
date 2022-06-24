e2cdd8f626ece7197bb9bfd9d44021637f6630f4
vikinatora <vikinatora@gmail.com>
2022-06-24 09:51:51 +0300

    enable eth health check

--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
+import { EthHealthIndicator } from '../eth-health/eth-health.service';
 import { DbHealthModule } from '../db-health/db-health.module';
 import { DbHealthService } from '../db-health/db-health.service';
+import { EthHealthModule } from '../eth-health/eth-health.module';
 import { EthereumModule } from '../ethereum/ethereum.module';
 import { HealthController } from './health.controller';
 
 @Module({
-  imports: [TerminusModule, EthereumModule, DbHealthModule],
-  providers: [DbHealthService],
+  imports: [TerminusModule, EthereumModule, DbHealthModule, EthHealthModule],
+  providers: [DbHealthService, EthHealthIndicator],
   controllers: [HealthController],
 })
 export class HealthModule {}
